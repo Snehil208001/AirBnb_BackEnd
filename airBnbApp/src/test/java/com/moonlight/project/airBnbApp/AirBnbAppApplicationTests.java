22fcc730ff1d1787f732065e3af37bf44f0aeff3
Snehil <snehilkumar94@gmail.com>
2026-02-01 10:15:55 +0530

    initial

---
+++ airBnbApp/src/test/java/com/moonlight/project/airBnbApp/AirBnbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonlight.project.airBnbApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBnbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
